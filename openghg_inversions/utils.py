95b76ff45b138a0fe0264e487ddf0f6250a376de
EricSaboya <ericsaboya54@gmail.com>
2022-11-09 12:13:03 +0000

    Added synomns function (copied from get_obs.py)

--- openghg_inversions/utils.py
+++ openghg_inversions/utils.py
@@ -6,7 +6,19 @@
 # About
 # Script containing common Python functions that can be called for running 
 # HBMCMC and InTEM inversion models. 
-# Many function have been copied form the acrg repo (e.g. acrg.name)
+# Most functions have been copied form the acrg repo (e.g. acrg.name)
+#
+# 
+# List of Functions (updated 9 Nov. 2022)
+# -----------------
+# - open_ds: function for opening xarray datasets
+# - filenames: function for providing name footprint filenames in specified
+#              period
+# - get_country (class)
+# - filtering
+# - areagrid
+# - boundary_conditions: reads in data for domain to compute BCs 
+# - read_netcdfs
 # ****************************************************************************
 
 import os 
@@ -26,29 +38,19 @@ from os.path import join
 from collections import OrderedDict
 
 
-# modules from name.py
-#import matplotlib as mpl
-#import matplotlib.pyplot as plt
-#from matplotlib.ticker import MaxNLocator
-#from matplotlib import ticker
-#import cartopy.crs as ccrs
-#import cartopy
 
 
 from acrg.time import convert
-import acrg.obs as obs
-from openghg_inversions.config.paths import Paths
-#from acrg.utils import is_number
 
-#from tqdm import tqdm
-#import dask.array as da
+from openghg_inversions.config.paths import Paths
+
 acrg_path = Paths.acrg
 data_path = Paths.data
 
+# ---- TO DO ----
+# -> import convert equiavalent (used in filenames)
 
 
-
-# **** Functions from name.py ****
 with open(acrg_path / "data/site_info.json") as f:
     site_info=json.load(f,object_pairs_hook=OrderedDict)
 
@@ -57,7 +59,7 @@ with open(acrg_path / "data/species_info.json") as f:
 
 def open_ds(path, chunks=None, combine=None):
     """
-    Function efficiently opens xray datasets.
+    Function efficiently opens xarray datasets.
 
     Args:
         path (str)
@@ -81,117 +83,157 @@ def open_ds(path, chunks=None, combine=None):
             ds.load()
     return ds
 
-
-def filenames(site, domain, start, end, height, fp_directory, met_model = None, network=None, species=None):
+def read_netcdfs(files, dim = "time", chunks=None, verbose=True):
     """
-    The filenames function outputs a list of available footprint file names,
-    for given site, domain, directory and date range.
-    
-    Expect filenames of the form:
-        [fp_directory]/domain/site*-height-species*domain*ym*.nc or [fp_directory]/domain/site*-height_domain*ym*.nc
-        e.g. /data/shared/LPDM/fp_NAME/EUROPE/HFD-UKV-100magl-rn_EUROPE_202012.nc or /data/shared/LPDM/fp_NAME/EUROPE/MHD-10magl_EUROPE_201401.nc 
+    The read_netcdfs function uses xarray to open sequential netCDF files and 
+    and concatenates them along the specified dimension.
+    Note: this function makes sure that file is closed after open_dataset call.
     
     Args:
-        site (str) : 
-            Site name. Full list of site names should be defined within data/site_info.json
-        domain (str) : 
-            Domain name. The footprint files should be sub-categorised by the NAME domain name.
-        start (str) : 
-            Start date in format "YYYY-MM-DD" for range of files to find.
-        end (str) : 
-            End date in same format as start for range of files to find.
-        height (str) : 
-            Height related to input data. 
-        fp_directory (str) :
-            fp_directory can be specified if files are not in the default directory must point to a directory 
-            which contains subfolders organized by domain.
-        met_model (str):
-            Met model used to run NAME
-            Default is None and implies the standard global met model
-            Alternates include 'UKV' and this met model must be in the outputfolder NAME        
-        network (str, optional) : 
-            Network for site. 
-            If not specified, first entry in data/site_info.json file will be used (if there are multiple).
-        species (str, optional)
-            If specified, will search for species specific footprint files.
+        files (list) : 
+            List of netCDF filenames.
+        dim (str, optional) : 
+            Dimension of netCDF to use for concatenating the files.
+            Default = "time".
+        chunks (dict)
+            size of chunks for each dimension
+            e.g. {'lat': 50, 'lon': 50}
+            opens dataset with dask, such that it is opened 'lazily'
+            and all of the data is not loaded into memory
+            defaults to None - dataset is opened with out dask
+    
     Returns:
-        list (str): matched filenames
+        xarray.Dataset : 
+            All files open as one concatenated xarray.Dataset object    
+    """
+    if verbose:
+        print("Reading and concatenating files: ")
+        for fname in files:
+            print(fname)
+
+    datasets = [open_ds(p, chunks=chunks) for p in sorted(files)]
+
+    # reindex all of the lat-lon values to a common one to prevent floating point error differences
+    with xr.open_dataset(files[0]) as temp:
+        fields_ds = temp.load()
+    fp_lat = fields_ds["lat"].values
+    fp_lon = fields_ds["lon"].values
+
+    datasets = [ds.reindex(indexers={"lat":fp_lat, "lon":fp_lon}, method="nearest", tolerance=1e-5) for ds in datasets]
+
+    combined = xr.concat(datasets, dim)
+    return combined
+
+
+def synonyms(search_string, info, alternative_label = "alt"):
+    '''
+    Check to see if there are other names that we should be using for
+    a particular input. E.g. If CFC-11 or CFC11 was input, go on to use cfc-11,
+    as this is used in species_info.json
+    
+    Args:
+        search_string (str): Input string that you're trying to match
+        info (dict): Dictionary whose keys are the "default" values, and an
+             variable that contains other possible names
+    Returns:
+        corrected string
+    '''
+
+    keys=list(info.keys())
+
+    #First test whether site matches keys (case insensitive)
+    out_strings = \
+        [k for k in keys if k.upper() == search_string.upper()]
+
+    #If not found, search synonyms
+    if len(out_strings) == 0:
+        for k in keys:
+            matched_strings = \
+                [s for s in info[k][alternative_label] \
+                    if s.upper() == search_string.upper()]
+            if len(matched_strings) != 0:
+                out_strings = [k]
+                break
+
+    if len(out_strings) == 1:
+        out_string = out_strings[0]
+    else:
+        out_string = None
+
+    return out_string
+
+
+def boundary_conditions(domain, species, start = None, end = None, bc_directory=None):
+    """
+    The boundary_conditions function reads in the files with the global model vmrs at the domain edges 
+    to give the boundary conditions as an xarray Dataset.
+
+    Expect filenames of the form:
+        [bc_directory]/domain/species.lower()_*.nc
+        e.g. [/data/shared/LPDM/bc]/EUROPE/ch4_EUROPE_201301.nc
+
+    Args:
+        domain (str) : 
+            Domain name. The boundary condition files should be sub-categorised by the domain.
+        species (str) : 
+            Species name. All species names are defined data/species_info.json.
+        start (str, optional) : 
+            Start date in format "YYYY-MM-DD" to output only a time slice of all the flux files.
+            The start date used will be the first of the input month. I.e. if "2014-01-06" is input,
+            "2014-01-01" will be used.  This is to mirror the time slice functionality of the filenames 
+            function.
+        end (str, optional): 
+            End date in same format as start to output only a time slice of all the flux files.
+            The end date used will be the first of the input month and the timeslice will go up
+            to, but not include, this time. I.e. if "2014-02-25' is input, "2014-02-01" will be used.
+            This is to mirror the time slice functionality of the filenames function.
+        bc_directory (str, optional) : 
+            bc_directory can be specified if files are not in the default directory. 
+            Must point to a directory which contains subfolders organized by domain.
+
+    Returns:
+        xarray.Dataset : 
+            Combined dataset of matching boundary conditions files
     """
 
-    # Read site info for heights
-    if height is None:
-        if not site in list(site_info.keys()):
-            print("Site code not found in data/site_info.json to get height information. " + \
-                  "Check that site code is as intended. "+ \
-                  "If so, either add new site to file or input height manually.")
-            return None
-        if network is None:
-            network = list(site_info[site].keys())[0]
-        height = site_info[site][network]["height_name"][0]
+    if bc_directory is None:
+        bc_directory = join(data_path, 'LPDM/bc/')
 
-    if species:
-        species_obs = obs.read.synonyms(species, species_info)   # <--------- Change obs.read.syn HERE! 
+    filenames = os.path.join(bc_directory,domain,f"{species.lower()}_*.nc")
 
-        if 'lifetime' in species_info[species_obs].keys():
-            lifetime = species_info[species_obs]["lifetime"]
-            lifetime_hrs = convert.convert_to_hours(lifetime)
-            # if a monthly lifetime is a list, use the minimum lifetime 
-            # in the list to determine whether a species specific footprint is needed
-            if type(lifetime) == list:
-                lifetime_hrs = min(lifetime_hrs)
-        else:
-            lifetime_hrs = None
-    else:
-        lifetime_hrs = None
+    files = sorted(glob.glob(filenames))
+    file_no_acc = [ff for ff in files if not os.access(ff, os.R_OK)]
+    files = [ff for ff in files if os.access(ff, os.R_OK)]
 
-    # Convert into time format
-    months = pd.date_range(start = start, end = end, freq = "M").to_pydatetime()
-    yearmonth = [str(d.year) + str(d.month).zfill(2) for d in months]
-
-    # first search for species specific footprint files.
-    # if does not exist, use integrated files if lifetime of species is over 2 months
-    # if lifetime under 2 months and no species specific file exists, fail
-
-    files = []
-    for ym in yearmonth:
-
-        if species:
-
-            if met_model:
-                f=glob.glob(join(fp_directory,domain,f"{site}-{height}_{met_model}_{species}_{domain}_{ym}*.nc"))
-            else:
-                f=glob.glob(join(fp_directory,domain,f"{site}-{height}_{species}_{domain}_{ym}*.nc"))
-
-
-        else:
-            #manually create empty list if no species specified
-            f = []
-
-        if len(f) == 0:
-
-            if met_model:
-                glob_path = join(fp_directory,domain,f"{site}-{height}_{met_model}_{domain}_{ym}*.nc")
-            else:
-                glob_path = join(fp_directory,domain,f"{site}-{height}_{domain}_{ym}*.nc")
-
-            if lifetime_hrs is None:
-                print("No lifetime defined in species_info.json or species not defined. WARNING: 30-day integrated footprint used without chemical loss.")
-                f=glob.glob(glob_path)
-            elif lifetime_hrs <= 1440:
-                print("This is a short-lived species. Footprints must be species specific. Re-process in process.py with lifetime")
-                return []
-            else:
-                print("Treating species as long-lived.")
-                f=glob.glob(glob_path)
-
-        if len(f) > 0:
-            files += f
-
-    files.sort()
+    if len(file_no_acc)>0:
+        print('Warning: unable to read all boundary conditions files which match this criteria:')
+        [print(ff) for ff in file_no_acc]
 
     if len(files) == 0:
-        print(f"Can't find footprints file: {glob_path}")
-    return files
+        print("Cannot find boundary condition files in {}".format(filenames))
+        raise IOError(f"\nError: Cannot find boundary condition files for domain '{domain}' and species '{species}' ")
+
+    bc_ds = read_netcdfs(files)
+
+    if start == None or end == None:
+        print("To get boundary conditions for a certain time period you must specify an end date.")
+        return bc_ds
+    else:
+        #Change timeslice to be the beginning and end of months in the dates specified.
+        start = pd.to_datetime(start)
+        month_start = dt.datetime(start.year, start.month, 1, 0, 0)
+
+        end = pd.to_datetime(end)
+        month_end = dt.datetime(end.year, end.month, 1, 0, 0) - \
+                    dt.timedelta(seconds = 1)
+
+        bc_timeslice = bc_ds.sel(time=slice(month_start, month_end))
+        if len(bc_timeslice.time)==0:
+            bc_timeslice = bc_ds.sel(time=start, method = 'ffill')
+            bc_timeslice = bc_timeslice.expand_dims('time',axis=-1)
+            print(f"No boundary conditions available during the time period specified so outputting\
+                    boundary conditions from {bc_timeslice.time.values[0]}")
+        return bc_timeslice
 
 
 class get_country(object):
@@ -432,23 +474,6 @@ def filtering(datasets_in, filters, keep_missing=False):
     return datasets
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Everything below here is good! 
-
 def areagrid(lat, lon):
   """Calculates grid of areas (m2) given arrays of latitudes and longitudes
 
@@ -488,4 +513,121 @@ def areagrid(lat, lon):
 
   return area
 
+
+
+
+
+
+# Filenams needs sorting still! 
+
+def filenames(site, domain, start, end, height, fp_directory, met_model = None, network=None, species=None):
+    """
+    The filenames function outputs a list of available footprint file names,
+    for given site, domain, directory and date range.
+    
+    Expect filenames of the form:
+        [fp_directory]/domain/site*-height-species*domain*ym*.nc or [fp_directory]/domain/site*-height_domain*ym*.nc
+        e.g. /data/shared/LPDM/fp_NAME/EUROPE/HFD-UKV-100magl-rn_EUROPE_202012.nc or /data/shared/LPDM/fp_NAME/EUROPE/MHD-10magl_EUROPE_201401.nc 
+    
+    Args:
+        site (str) : 
+            Site name. Full list of site names should be defined within data/site_info.json
+        domain (str) : 
+            Domain name. The footprint files should be sub-categorised by the NAME domain name.
+        start (str) : 
+            Start date in format "YYYY-MM-DD" for range of files to find.
+        end (str) : 
+            End date in same format as start for range of files to find.
+        height (str) : 
+            Height related to input data. 
+        fp_directory (str) :
+            fp_directory can be specified if files are not in the default directory must point to a directory 
+            which contains subfolders organized by domain.
+        met_model (str):
+            Met model used to run NAME
+            Default is None and implies the standard global met model
+            Alternates include 'UKV' and this met model must be in the outputfolder NAME        
+        network (str, optional) : 
+            Network for site. 
+            If not specified, first entry in data/site_info.json file will be used (if there are multiple).
+        species (str, optional)
+            If specified, will search for species specific footprint files.
+    Returns:
+        list (str): matched filenames
+    """
+
+    # Read site info for heights
+    if height is None:
+        if not site in list(site_info.keys()):
+            print("Site code not found in data/site_info.json to get height information. " + \
+                  "Check that site code is as intended. "+ \
+                  "If so, either add new site to file or input height manually.")
+            return None
+        if network is None:
+            network = list(site_info[site].keys())[0]
+        height = site_info[site][network]["height_name"][0]
+
+    if species:
+        species_obs = synonyms(species, species_info)    
+
+        if 'lifetime' in species_info[species_obs].keys():
+            lifetime = species_info[species_obs]["lifetime"]
+            lifetime_hrs = convert.convert_to_hours(lifetime)
+            # if a monthly lifetime is a list, use the minimum lifetime 
+            # in the list to determine whether a species specific footprint is needed
+            if type(lifetime) == list:
+                lifetime_hrs = min(lifetime_hrs)
+        else:
+            lifetime_hrs = None
+    else:
+        lifetime_hrs = None
+
+    # Convert into time format
+    months = pd.date_range(start = start, end = end, freq = "M").to_pydatetime()
+    yearmonth = [str(d.year) + str(d.month).zfill(2) for d in months]
+
+    # first search for species specific footprint files.
+    # if does not exist, use integrated files if lifetime of species is over 2 months
+    # if lifetime under 2 months and no species specific file exists, fail
+
+    files = []
+    for ym in yearmonth:
+
+        if species:
+
+            if met_model:
+                f=glob.glob(join(fp_directory,domain,f"{site}-{height}_{met_model}_{species}_{domain}_{ym}*.nc"))
+            else:
+                f=glob.glob(join(fp_directory,domain,f"{site}-{height}_{species}_{domain}_{ym}*.nc"))
+
+
+        else:
+            #manually create empty list if no species specified
+            f = []
+
+        if len(f) == 0:
+
+            if met_model:
+                glob_path = join(fp_directory,domain,f"{site}-{height}_{met_model}_{domain}_{ym}*.nc")
+            else:
+                glob_path = join(fp_directory,domain,f"{site}-{height}_{domain}_{ym}*.nc")
+
+            if lifetime_hrs is None:
+                print("No lifetime defined in species_info.json or species not defined. WARNING: 30-day integrated footprint used without chemical loss.")
+                f=glob.glob(glob_path)
+            elif lifetime_hrs <= 1440:
+                print("This is a short-lived species. Footprints must be species specific. Re-process in process.py with lifetime")
+                return []
+            else:
+                print("Treating species as long-lived.")
+                f=glob.glob(glob_path)
+
+        if len(f) > 0:
+            files += f
+
+    files.sort()
+
+    if len(files) == 0:
+        print(f"Can't find footprints file: {glob_path}")
+    return files
                                                                                                                                                                                                                  
