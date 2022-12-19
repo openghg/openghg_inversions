6c51b168fe90e3549db3a56fb8f138ae619a2b89
EricSaboya <ericsaboya54@gmail.com>
2022-12-19 11:09:57 +0000

    Added information on each function

--- openghg_inversions/hbmcmc/inversionsetup.py
+++ openghg_inversions/hbmcmc/inversionsetup.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  2 12:54:43 2020
+# ****************************************************************************
+# Created: 7 Nov. 2022
+# Author: Eric Saboya, School of Geographical Sciences, University of Bristol
+# Contact: eric.saboya@bristol.ac.uk
+# ****************************************************************************
+# Originally createdby Luke Western (ACRG) and updated by Eric Saboya
+# 
+# About
+#   Functions used for setting up HBMCMC inversions 
+#
+# ****************************************************************************
 
-@author: lw13938
-
-Updated by Eric Saboya
-"""
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -15,48 +18,50 @@ from openghg.retrieve import get_obs_surface
 def opends(fn):
     '''
     Open a netcdf dataset with xarray
+    -----------------------------------
     Args:
-        fn (string)
-            netcdf file to be opened
+      fn (str):
+        Netcdf file to be opened
+
     Returns:
         xarray.Dataset: 
             netcdf file as  dataset 
+    -----------------------------------
     '''
     with xr.open_dataset(fn) as load:
         ds = load.load()
+
         return ds
 
 def addaveragingerror(fp_all, sites, species, start_date, end_date, meas_period,  
                       inlet=None, instrument=None):
-    """
+    '''
     Adds the variablility within the averaging period to the mole fraction error.
-    
+    -----------------------------------
     Args:
-        fp_all (dict):
-            Output from footprint_data_merge
-        sites (list):
-            List of site names
-        species (str):
-            Species of interest
-        start_date (str):
-            Start time of inversion "YYYY-mm-dd"
-        end_date (str):
-            End time of inversion "YYYY-mm-dd"
-        meas_period (list):
-            Averaging period of measurements
-        inlet (str/list, optional):
-            Specific inlet height for the site (must match number of sites).
-        instrument (str/list, optional):
-            Specific instrument for the site (must match number of sites).
-        obs_directory (str, optional):
-            Directory containing the obs data (with site codes as subdirectories)
-            if not default.    
+      fp_all (dict):
+        Output from ModelScenario
+      sites (list):
+        List of site names
+      species (str):
+        Species of interest
+      start_date (str):
+        Start time of inversion "YYYY-mm-dd"
+      end_date (str):
+        End time of inversion "YYYY-mm-dd"
+      meas_period (list):
+        Averaging period of measurements
+      inlet (list, optional):
+        Specific inlet height for the site (must match number of sites).
+      instrument (list):
+        Specific instrument for the site (must match number of sites).
             
     Returns:
-        fp_all (dict):
-            fp_all from input with averaging error added to the mole fraction
-            error
-    """
+      fp_all (dict):
+        fp_all from input with averaging error added to the mole fraction
+        error
+    -----------------------------------
+    '''
     #Add variability in measurement averaging period to repeatability 
     for i, site in enumerate(sites):
         get_obs = get_obs_surface(site=site,
@@ -83,117 +88,122 @@ def addaveragingerror(fp_all, sites, species, start_date, end_date, meas_period,
             fp_all[site.upper()].mf_repeatability.values = np.squeeze(np.sqrt(fp_all[site.upper()].mf_repeatability.values**2 + sitedataerr.resample(meas_period[i]).std(ddof=0).dropna().values.T**2))
         else:
             print('No mole fraction error information available in {}.'.format('fp_all'+str([site])))
+
     return fp_all
 
 def monthly_bcs(start_date, end_date, site, fp_data):
-    """
-    Creates a sensitivity matrix (H-matrix) for the boundary conditions, which
-    will map monthly boundary condition scalings to the observations. This is 
-    for a single site.
-    
+    '''
+    Creates a sensitivity matrix (H-matrix) for the boundary 
+    conditions, which will map monthly boundary condition 
+    scalings to the observations. This is for a single site.
+    -----------------------------------
     Args:
-        start_date (str):
-            Start time of inversion "YYYY-mm-dd"
-        end_date (str):
-            End time of inversion "YYYY-mm-dd"
-        site (str):
-            Site that you're creating it for
-        fp_data (dict):
-            Output from acrg_name.bc_sensitivity
+      start_date (str):
+        Start time of inversion "YYYY-mm-dd"
+      end_date (str):
+        End time of inversion "YYYY-mm-dd"
+      site (str):
+        Site that you're creating it for
+      fp_data (dict):
+        Output from utils..bc_sensitivity
             
     Returns:
-        Hmbc (array):
-            Sensitivity matrix by month for observations
-            
-    """
+      hmbc (array):
+        Sensitivity matrix by month for observations
+    -----------------------------------        
+    '''
     allmonth= pd.date_range(start_date, end_date, freq="MS")[:-1] 
     nmonth = len(allmonth)
     curtime = pd.to_datetime(fp_data[site].time.values).to_period("M")
     pmonth = pd.to_datetime(fp_data[site].resample(time="MS").mean().time.values)
-    Hmbc = np.zeros((4*nmonth, len(fp_data[site].time.values)))
-    cnt=0
+    hmbc = np.zeros((4*nmonth, len(fp_data[site].time.values)))
+    count=0
     for cord in range(4):
         for m in range(0,nmonth):
             if allmonth[m] not in pmonth:
-                cnt += 1
+                count += 1
                 continue
             mnth = allmonth[m].month
             yr = allmonth[m].year
             mnthloc = np.where(np.logical_and(curtime.month == mnth,curtime.year == yr))[0]
-            Hmbc[cnt,mnthloc] = fp_data[site].H_bc.values[cord,mnthloc] 
-            cnt += 1
-    return Hmbc
+            hmbc[cnt,mnthloc] = fp_data[site].H_bc.values[cord,mnthloc] 
+            count += 1
+
+    return hmbc
 
 def create_bc_sensitivity(start_date, end_date, site, fp_data, freq):
-    """
-    Creates a sensitivity matrix (H-matrix) for the boundary conditions, which
-    will map boundary condition scalings to the observations. This is 
-    for a single site. The frequency that the boundary condition sensitivity
-    is specifiec over must be specified in days. Currently only works 
-    for a boundary condition from each cardinal direction.
-    
+    '''
+    Creates a sensitivity matrix (H-matrix) for the boundary 
+    conditions, which will map boundary condition scalings to 
+    the observations. This is for a single site. The frequency 
+    that the boundary condition sensitivity is specified over 
+    must be given in days. Currently only works for a 
+    boundary condition from each cardinal direction.
+    -----------------------------------
     Args:
-        start_date (str):
-            Start time of inversion "YYYY-mm-dd"
-        end_date (str):
-            End time of inversion "YYYY-mm-dd"
-        site (str):
-            Site that you're creating it for
-        fp_data (dict):
-            Output from acrg_name.bc_sensitivity
-        freq (str):
-            Length-scale over which boundary condition sensitivities are
-            specified over. Specified as in pandas, e.g. "30D".
+      start_date (str):
+        Start time of inversion "YYYY-mm-dd"
+      end_date (str):
+        End time of inversion "YYYY-mm-dd"
+      site (str):
+        Site that you're creating it for
+      fp_data (dict):
+        Output from ModelScenario()
+      freq (str):
+        Length-scale over which boundary condition sensitivities are
+        specified over. Specified as in pandas, e.g. "30D".
             
     Returns:
-        Hmbc (array):
-            Sensitivity matrix by for observations to boundary conditions
-            
-    """
+      hmbc (array):
+        Sensitivity matrix by for observations to boundary conditions
+    -----------------------------------        
+    '''
     dys = int("".join([s for s in freq if s.isdigit()]))
-    alldates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date)+ pd.DateOffset(days=dys), freq=freq)
+    alldates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date)+pd.DateOffset(days=dys), freq=freq)
     ndates = np.sum(alldates < pd.to_datetime(end_date))
     curdates = fp_data[site].time.values
-    Hmbc = np.zeros((4*ndates, len(fp_data[site].time.values)))
-    cnt=0
+    hmbc = np.zeros((4*ndates, len(fp_data[site].time.values)))
+    count=0
     for cord in range(4):
         for m in range(0,ndates):
             dateloc = np.where(np.logical_and(curdates >= alldates[m].to_datetime64(),curdates < alldates[m+1].to_datetime64()))[0]
             if len(dateloc) == 0:
-                cnt += 1
+                count += 1
                 continue
-            Hmbc[cnt,dateloc] = fp_data[site].H_bc.values[cord,dateloc] 
-            cnt += 1
-    return Hmbc
+            hmbc[cnt,dateloc] = fp_data[site].H_bc.values[cord,dateloc] 
+            count += 1
 
-def sigma_freq_indicies(Ytime, sigma_freq):
-    """
-    Create an index that splits times into given periods
-    
+    return hmbc
+
+def sigma_freq_indicies(ytime, sigma_freq):
+    '''
+    Create an index that splits times 
+    into given periods
+    -----------------------------------
     Args:
-        Ytime (array of datetime64):
-            concatanted array of time values for observations
-        sigma_freq (str):
-            either "monthly", a pandas format string ("30D"), or None
-            this is the period of time to divide the time array into
+      ytime (array of datetime64):
+        concatanted array of time values for observations
+      sigma_freq (str):
+        either "monthly", a pandas format string ("30D"), or None
+        this is the period of time to divide the time array into
             
     Returns:
-        output (array):
-            index array that defines periods against time
-    """
-    
-    dt = pd.to_datetime(Ytime)
-    output = np.zeros(shape = len(Ytime)).astype(int)
+      output (array):
+        index array that defines periods against time
+    -----------------------------------
+    ''' 
+    ydt = pd.to_datetime(ytime)
+    output = np.zeros(shape = len(ytime)).astype(int)
     if sigma_freq == None:
-        #output already all 0's as expected for this setting
+        # output already all 0's as expected for this setting
         pass
     elif sigma_freq.lower() == "monthly":
-        months = dt.month
-        years = dt.year
+        months = ydt.month
+        years = ydt.year
         months_u = np.unique(months)
         years_u = np.unique(years)
         
-        #incrementally set sigma indicies for each month in each year
+        # incrementally set sigma indicies for each month in each year
         count=0      
         for y in years_u:
             for m in months_u:
@@ -201,21 +211,22 @@ def sigma_freq_indicies(Ytime, sigma_freq):
                 output[indicies] = count
                 count += 1
     else:
-        #divide the time between t0 and ti by sigma_freq, then floor to calculate number of integer intervals
-        #the calculation is performed in seconds as division by pd time_delta is not allowed
+        # divide the time between t0 and ti by sigma_freq, then floor 
+        # to calculate number of integer intervals the calculation is 
+        # performed in seconds as division by pd time_delta is not allowed
         time_delta = pd.to_timedelta(sigma_freq)
-        fractional_freq_time = (dt - np.amin(dt)).total_seconds() / time_delta.total_seconds()
+        fractional_freq_time = (ydt - np.amin(ydt)).total_seconds() / time_delta.total_seconds()
         output[:] =np.floor(fractional_freq_time.values).astype(int)
                 
     return output
         
 def offset_matrix(siteindicator):
-    """
+    '''
     Set up a matrix that can be used to add an offset to each site.
     This will anchor to the first site (i.e. first site has no offset)
-    """
-    B = np.zeros((int(len(siteindicator)), int(max(siteindicator))+1))
+    '''
+    b = np.zeros((int(len(siteindicator)), int(max(siteindicator))+1))
     for i in range(int(max(siteindicator)+1)):
-        B[siteindicator == i,i] = 1. 
-    return B
-    
+        b[siteindicator == i,i] = 1. 
+
+    return b    
