8e9f97e9627e294081b4bc4e9a95611d31816136
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-01-06 17:22:53 +0000

    Allow list of obs and fp stores
    Also, skip a site if obs or fp not found.

--- openghg_inversions/get_data.py
+++ openghg_inversions/get_data.py
@@ -4,9 +4,9 @@
 # Contact: eric.saboya@bristol.ac.uk
 # *****************************************************************************
 # About
-# Different functions for retrieving appropriate datasets for forward 
+# Different functions for retrieving appropriate datasets for forward
 # simulations
-# 
+#
 # Current options include:
 # - "data_processing_surface_notracer": Surface based measurements, without tracers
 # - "data_processing_surface_tracer"  : Surface based measurements, with tracers
@@ -25,17 +25,35 @@ from openghg.retrieve import get_obs_surface, get_flux
 from openghg.retrieve import get_bc, get_footprint
 from openghg.analyse import ModelScenario
 from openghg.dataobjects import BoundaryConditionsData
+from openghg.types import SearchError
 
-def data_processing_surface_notracer(species, sites, domain, averaging_period, start_date, end_date,
-                                     met_model=None, fp_model="NAME", fp_height=None,
-                                     emissions_name=None, inlet=None, instrument=None, bc_input=None,
-                                     bc_store=None, obs_store=None, footprint_store=None, emissions_store=None,
-                                     averagingerror=True,save_merged_data=False,
-                                     merged_data_name=None,merged_data_dir=None):
 
+def data_processing_surface_notracer(
+    species,
+    sites,
+    domain,
+    averaging_period,
+    start_date,
+    end_date,
+    met_model=None,
+    fp_model="NAME",
+    fp_height=None,
+    emissions_name=None,
+    inlet=None,
+    instrument=None,
+    bc_input=None,
+    bc_store=None,
+    obs_store=None,
+    footprint_store=None,
+    emissions_store=None,
+    averagingerror=True,
+    save_merged_data=False,
+    merged_data_name=None,
+    merged_data_dir=None,
+):
     """
     Retrieve and prepare fixed-surface datasets from
-    specified OpenGHG object stores for forward 
+    specified OpenGHG object stores for forward
     simulations that do not use tracers
     ---------------------------------------------
     Args:
@@ -44,13 +62,13 @@ def data_processing_surface_notracer(species, sites, domain, averaging_period, s
             e.g. "co2"
         sites (list/str):
             List of strings containing measurement
-            station/site abbreviations 
+            station/site abbreviations
             e.g. ["MHD", "TAC"]
         domain (str):
             Model domain region of interest
             e.g. "EUROPE"
         averaging_period (list/str):
-            List of averaging periods to apply to 
+            List of averaging periods to apply to
             mole fraction data. NB. len(averaging_period)==len(sites)
             e.g. ["1H", "1H"]
         start_date (str):
@@ -69,10 +87,10 @@ def data_processing_surface_notracer(species, sites, domain, averaging_period, s
             List of keywords args associated with emissions files
             in the object store.
         inlet (str/list, optional):
-            Specific inlet height for the site observations. 
+            Specific inlet height for the site observations.
             (length must match number of sites)
         instrument (str/list, optional):
-            Specific instrument for the site 
+            Specific instrument for the site
             (length must match number of sites).
         bc_store (str):
             Name of object store to retrieve boundary conditions data from
@@ -82,7 +100,7 @@ def data_processing_surface_notracer(species, sites, domain, averaging_period, s
             Name of object store to retrieve footprints data from
         emissions_store (str):
             Name of object store to retrieve emissions data from
-        averagingerror (bool/opt): 
+        averagingerror (bool/opt):
           Adds the variability in the averaging period to the measurement
           error if set to True.
 
@@ -90,167 +108,206 @@ def data_processing_surface_notracer(species, sites, domain, averaging_period, s
 
     # Change list of sites to upper case equivalent as
     # most functions use upper case notation
-    for i, site in enumerate(sites): sites[i]=site.upper()
+    sites = [site.upper() for site in sites]
 
-    fp_all={}
-    fp_all['.species']=species.upper()
+    fp_all = {}
+    fp_all[".species"] = species.upper()
 
     # Get fluxes
-    flux_dict={}
+    flux_dict = {}
     for source in emissions_name:
-        get_flux_data = get_flux(species=species,
-                                 domain=domain,
-                                 source=source,
-                                 start_date=start_date,
-                                 end_date=end_date,
-                                 store=emissions_store)
+        get_flux_data = get_flux(
+            species=species,
+            domain=domain,
+            source=source,
+            start_date=start_date,
+            end_date=end_date,
+            store=emissions_store,
+        )
 
-        flux_dict[source]=get_flux_data
-    fp_all['.flux']=flux_dict
+        flux_dict[source] = get_flux_data
+    fp_all[".flux"] = flux_dict
 
-    footprint_dict={}
-    scales={}
-    check_scales=[]
+    footprint_dict = {}
+    scales = {}
+    check_scales = []
+
+    if not isinstance(obs_store, list):
+        obs_store = [obs_store]
+
+    if not isinstance(footprint_store, list):
+        footprint_store = [footprint_store]
 
     for i, site in enumerate(sites):
         # Get observations
-        site_data=get_obs_surface(site=site,
-                                  species=species.lower(),
-                                  inlet=inlet[i],
-                                  start_date=start_date,
-                                  end_date=end_date,
-                                  average=averaging_period[i],
-                                  instrument=instrument[i],
-                                  store=obs_store)
+        obs_found = False
+        for store in obs_store:
+            try:
+                site_data = get_obs_surface(
+                    site=site,
+                    species=species.lower(),
+                    inlet=inlet[i],
+                    start_date=start_date,
+                    end_date=end_date,
+                    average=averaging_period[i],
+                    instrument=instrument[i],
+                    store=store,
+                )
+            except SearchError:
+                continue  # try next store
+            else:
+                unit = float(site_data[site].mf.units)
+                obs_found = True
+                break  # stop checking stores
 
-        unit=float(site_data[site].mf.units)
+        # if obs not found, skip this site
+        if not obs_found:
+            continue
 
         # Get footprints
-        get_fps = get_footprint(site=site,
-                                height=fp_height[i],
-                                domain=domain,
-                                model=fp_model,
-                                start_date=start_date,
-                                end_date=end_date,
-                                store=footprint_store)
-        footprint_dict[site] = get_fps
+        footprint_found = False
+        for store in footprint_store:
+            try:
+                get_fps = get_footprint(
+                    site=site,
+                    height=fp_height[i],
+                    domain=domain,
+                    model=fp_model,
+                    start_date=start_date,
+                    end_date=end_date,
+                    store=store,
+                )
+            except SearchError:
+                continue  # try next store
+            else:
+                footprint_dict[site] = get_fps
+                footprint_found = True
+                break  # stop checking stores
 
+        # if footprint not found, skip this site
+        if not footprint_found:
+            continue
 
         # Get boundary conditions
-        get_bc_data = get_bc(species=species,
-                             domain=domain,
-                             bc_input=bc_input,
-                             start_date=start_date,
-                             end_date=end_date,
-                             store=bc_store)
+        if ".bc" not in fp_all:
+            get_bc_data = get_bc(
+                species=species,
+                domain=domain,
+                bc_input=bc_input,
+                start_date=start_date,
+                end_date=end_date,
+                store=bc_store,
+            )
 
-
-        # Divide by trace gas species units
-        # See if R+G can include this 'behind the scenes'
-        get_bc_data.data.vmr_n.values = get_bc_data.data.vmr_n.values/unit
-        get_bc_data.data.vmr_e.values = get_bc_data.data.vmr_e.values/unit
-        get_bc_data.data.vmr_s.values = get_bc_data.data.vmr_s.values/unit
-        get_bc_data.data.vmr_w.values = get_bc_data.data.vmr_w.values/unit
-        my_bc = BoundaryConditionsData(get_bc_data.data.transpose("height","lat","lon","time"),
-                                           get_bc_data.metadata)
-        fp_all['.bc']=my_bc
+            # Divide by trace gas species units
+            # See if R+G can include this 'behind the scenes'
+            get_bc_data.data.vmr_n.values = get_bc_data.data.vmr_n.values / unit
+            get_bc_data.data.vmr_e.values = get_bc_data.data.vmr_e.values / unit
+            get_bc_data.data.vmr_s.values = get_bc_data.data.vmr_s.values / unit
+            get_bc_data.data.vmr_w.values = get_bc_data.data.vmr_w.values / unit
+            my_bc = BoundaryConditionsData(
+                get_bc_data.data.transpose("height", "lat", "lon", "time"), get_bc_data.metadata
+            )
+            fp_all[".bc"] = my_bc
 
         # Create ModelScenario object for all emissions_sectors
         # and combine into one object
         if len(emissions_name) == 1:
-            model_scenario=ModelScenario(site=site,
-                                         species=species,
-                                         inlet=inlet[i],
-                                         start_date=start_date,
-                                         end_date=end_date,
-                                         obs=site_data,
-                                         footprint=footprint_dict[site],
-                                         flux=flux_dict,
-                                         bc=my_bc)
+            model_scenario = ModelScenario(
+                site=site,
+                species=species,
+                inlet=inlet[i],
+                start_date=start_date,
+                end_date=end_date,
+                obs=site_data,
+                footprint=footprint_dict[site],
+                flux=flux_dict,
+                bc=my_bc,
+            )
 
-            scenario_combined=model_scenario.footprints_data_merge()
+            scenario_combined = model_scenario.footprints_data_merge()
             scenario_combined.bc_mod.values = scenario_combined.bc_mod.values * unit
 
-        elif len(emissions_name) >1:
+        elif len(emissions_name) > 1:
             model_scenario_dict = {}
- 
+
             for source in emissions_sources:
-                model_scenario=ModelScenario(site=site,
-                                             species=species,
-                                             inlet=inlet[i],
-                                             start_date=start_date,
-                                             end_date=end_date,
-                                             obs=site_data,
-                                             footprint=footprint_dict[site],
-                                             flux=flux_dict,
-                                             bc=my_bc)
+                model_scenario = ModelScenario(
+                    site=site,
+                    species=species,
+                    inlet=inlet[i],
+                    start_date=start_date,
+                    end_date=end_date,
+                    obs=site_data,
+                    footprint=footprint_dict[site],
+                    flux=flux_dict,
+                    bc=my_bc,
+                )
 
-                scenario_sector=model_scenario.footprints_data_merge(sources=source)
-                model_scenario_dict["mf_mod_high_res_"+source] = scenario_sector["mf_mod_high_res"]       
+                scenario_sector = model_scenario.footprints_data_merge(sources=source)
+                model_scenario_dict["mf_mod_high_res_" + source] = scenario_sector["mf_mod_high_res"]
 
-            model_scenario=ModelScenario(site=site,
-                                         species=species,
-                                         inlet=inlet[i],
-                                         start_date=start_date,
-                                         end_date=end_date,
-                                         obs=site_data,
-                                         footprint=footprint_dict[site],
-                                         flux=flux_dict,
-                                         bc=my_bc)
+            model_scenario = ModelScenario(
+                site=site,
+                species=species,
+                inlet=inlet[i],
+                start_date=start_date,
+                end_date=end_date,
+                obs=site_data,
+                footprint=footprint_dict[site],
+                flux=flux_dict,
+                bc=my_bc,
+            )
 
-            scenario_combined=model_scenario.footprints_data_merge()
+            scenario_combined = model_scenario.footprints_data_merge()
 
             for key in model_scenario_dict.keys():
                 scenario_combined[key] = model_scenario_dict[key]
-            
+
             scenario_combined.bc_mod.values = scenario_combined.bc_mod.values * unit
 
-        fp_all[site]=scenario_combined
+        fp_all[site] = scenario_combined
 
         # Check consistency of measurement scales between sites
-        check_scales+=[scenario_combined.scale]
-        if not all (s==check_scales[0] for s in check_scales):
-            rt=[]
+        check_scales += [scenario_combined.scale]
+        if not all(s == check_scales[0] for s in check_scales):
+            rt = []
             for i in check_scales:
-                if isinstance(i, list): rt.extend(flatten(i))
+                if isinstance(i, list):
+                    rt.extend(flatten(i))
             else:
                 rt.append(i)
-            scales[site]=rt
+            scales[site] = rt
         else:
-            scales[site]=check_scales[0]
+            scales[site] = check_scales[0]
 
-    fp_all['.scales']=scales
-    fp_all['.units']=float(scenario_combined.mf.units)
+    fp_all[".scales"] = scales
+    fp_all[".units"] = float(scenario_combined.mf.units)
 
     # If site contains measurement errors given as repeatability and variability,
     # use variability to replace missing repeatability values, then drop variability
     for site in sites:
         if "mf_variability" in fp_all[site] and "mf_repeatability" in fp_all[site]:
-            fp_all[site]["mf_repeatability"][np.isnan(fp_all[site]["mf_repeatability"])] = \
-                fp_all[site]["mf_variability"][np.logical_and(np.isfinite(fp_all[site]["mf_variability"]),np.isnan(fp_all[site]["mf_repeatability"]) )]
+            fp_all[site]["mf_repeatability"][np.isnan(fp_all[site]["mf_repeatability"])] = fp_all[site][
+                "mf_variability"
+            ][
+                np.logical_and(
+                    np.isfinite(fp_all[site]["mf_variability"]), np.isnan(fp_all[site]["mf_repeatability"])
+                )
+            ]
             fp_all[site] = fp_all[site].drop_vars("mf_variability")
 
     # Add measurement variability in averaging period to measurement error
     if averagingerror:
-        fp_all = setup.addaveragingerror(fp_all,
-                                         sites,
-                                         species,
-                                         start_date,
-                                         end_date,
-                                         averaging_period,
-                                         inlet=inlet,
-                                         instrument=instrument)
-        
+        fp_all = setup.addaveragingerror(
+            fp_all, sites, species, start_date, end_date, averaging_period, inlet=inlet, instrument=instrument
+        )
+
     if save_merged_data == True:
-        fp_out = open(merged_data_dir+merged_data_name, 'wb')
+        fp_out = open(merged_data_dir + merged_data_name, "wb")
         pickle.dump(fp_all, fp_out)
         fp_out.close()
 
-        print(f'\nfp_all saved in {merged_data_dir}\n')
+        print(f"\nfp_all saved in {merged_data_dir}\n")
 
     return fp_all
-
-
-
-
-
