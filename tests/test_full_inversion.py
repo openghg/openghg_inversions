20c38b0839cd71ca74f8e85aecb30f3be5156fb4
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-02-12 16:52:05 +0000

    Fixup: forgot to change variable name

--- tests/test_full_inversion.py
+++ tests/test_full_inversion.py
@@ -5,7 +5,7 @@ from openghg_inversions.hbmcmc.hbmcmc import fixedbasisMCMC
 
 @pytest.fixture
 def mcmc_args(tmp_path, tac_ch4_data_args, merged_data_dir, pickled_data_file_name):
-    mcmc_args = data_args.copy()
+    mcmc_args = tac_ch4_data_args.copy()
     mcmc_args.update(
         {
             "outputname": "test_run",
