7fc3f1b2e5d88d0fd0901e4d44848d78825aae93
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-02-19 11:00:33 +0000

    Moved helper functions to _functions submodule

--- openghg_inversions/basis/_functions.py
+++ openghg_inversions/basis/_functions.py
@@ -1,1 +1,48 @@
-#!/usr/bin/env python
+from typing import cast, Optional, Union
+
+import numpy as np
+import xarray as xr
+
+
+def _flux_fp_from_fp_all(
+    fp_all: dict, emissions_name: Optional[list[str]] = None
+) -> tuple[xr.DataArray, list[xr.DataArray]]:
+    """Get flux and list of footprints from `fp_all` dictionary and optional list of emissions names."""
+    if emissions_name is not None:
+        flux = fp_all[".flux"][emissions_name[0]].data.flux
+    else:
+        first_flux = next(iter(fp_all[".flux"].values()))
+        flux = first_flux.data.flux
+
+    flux = cast(xr.DataArray, flux)
+
+    footprints: list[xr.DataArray] = [v.fp for k, v in fp_all.items() if not k.startswith(".")]
+
+    return flux, footprints
+
+
+def _mean_fp_times_mean_flux(
+    flux: xr.DataArray,
+    footprints: list[xr.DataArray],
+    abs_flux: bool = False,
+    mask: Optional[xr.DataArray] = None,
+) -> xr.DataArray:
+    if abs_flux is True:
+        print("Using absolute value of flux array.")
+        flux = abs(flux)
+
+    mean_flux = flux.mean("time")
+
+    fp_total = sum(footprints)
+    n_measure = sum(len(fp.time) for fp in footprints)
+
+    fp_total = cast(xr.DataArray, fp_total)  # otherwise mypy complains about the next line
+    mean_fp = fp_total.sum("time") / n_measure
+
+    if mask is not None:
+        # align to footprint lat/lon
+        mean_fp, mean_flux, mask = xr.align(mean_fp, mean_flux, mask, join="override")
+        return (mean_fp * mean_flux).where(mask, drop=True)
+
+    mean_fp, mean_flux = xr.align(mean_fp, mean_flux, join="override")
+    return (mean_fp * mean_flux)
