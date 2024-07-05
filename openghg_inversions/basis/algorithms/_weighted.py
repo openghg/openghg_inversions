fc5be2a94a503aed56116d77c7e79b178d52e72e
alexdanjou <158201791+alexdanjou@users.noreply.github.com>
2024-07-05 21:00:50 +0100

    Update openghg_inversions/basis/algorithms/_weighted.py
    Co-authored-by: Brendan Murphy <git@mellotron.anonaddy.com>

--- openghg_inversions/basis/algorithms/_weighted.py
+++ openghg_inversions/basis/algorithms/_weighted.py
@@ -84,7 +84,7 @@ def get_nregions(bucket : float,
             data, spatial distribution of bakeries, you chose!
 
     Return :
-        no. of basis functions for bucket value
+        number of basis functions for bucket value
     """
     return np.max(bucket_split_landsea_basis(grid, bucket))
 
