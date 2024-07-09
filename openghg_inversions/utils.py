1e1fe8594eeed6b42c043b9053ee190067f73778
alexdanjou <158201791+alexdanjou@users.noreply.github.com>
2024-07-09 11:50:02 +0100

    Update utils.py

--- openghg_inversions/utils.py
+++ openghg_inversions/utils.py
@@ -311,8 +311,10 @@ def basis_boundary_conditions(domain: str, basis_case: str, bc_basis_directory: 
                 f"Default BC basis directory {bc_basis_directory} was empty. "
                 "Add basis files or specify `bc_basis_directory`."
             )
-
-    file_path = Path(f'{bc_basis_directory}/{domain}').glob(f"{basis_case}_{domain}*.nc")
+    else:
+        bc_basis_directory = Path(bc_basis_directory)
+        
+    file_path = (bc_basis_directory / domain).glob(f"{basis_case}_{domain}*.nc")
     files = sorted(list(file_path))
 
     # check for files that we can't access
