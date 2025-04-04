7baa58a02466574eb2df5c96e0642af6975072e1
Alexandre Danjou <alexandre.danjou.pro@gmail.com>
2025-04-04 16:05:59 +0100

    Add inlet in the keep_variables for the case when the inlet is derive from a slice. Needed when looking for footprint with parameter fp_height="auto"

--- openghg_inversions/inversion_data/get_data.py
+++ openghg_inversions/inversion_data/get_data.py
@@ -307,7 +307,8 @@ def data_processing_surface_notracer(
     keep_variables = [f"{species}",
                         f"{species}_variability", 
                         f"{species}_repeatability",
-                        f"{species}_number_of_observations"]
+                        f"{species}_number_of_observations",
+                        "inlet"]
     warnings.warn(f"Dropping all variables besides {keep_variables}")
     for i, site in enumerate(sites):
         # Get observations data
