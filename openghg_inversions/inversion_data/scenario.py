cfd25a9bd483d59c0b3bd684af2bf6abffb79f11
SutarPrasad <vq21425@bristol.ac.uk>
2025-06-06 12:08:03 +0100

    merged_scenario updated to check for satellite

--- openghg_inversions/inversion_data/scenario.py
+++ openghg_inversions/inversion_data/scenario.py
@@ -10,16 +10,27 @@ def merged_scenario_data(
     flux_dict: dict[str, FluxData],
     bc_data: BoundaryConditionsData | None = None,
     platform: str | None = None,
+    max_level: int | None = None
 ) -> xr.Dataset:
     """Create ModelScenario and get result of `footprint_data_merge`."""
     # Create ModelScenario object for all emissions_sectors
     # and combine into one object
-    model_scenario = ModelScenario(
-        obs=obs_data,
-        footprint=footprint_data,
-        flux=flux_dict,
-        bc=bc_data,
-    )
+    if platform is not None and "satellite" in platform:
+        model_scenario = ModelScenario(
+            obs_column=obs_data,
+            footprint=footprint_data,
+            flux=flux_dict,
+            bc=bc_data,
+            platform=platform,
+            max_level=max_level
+        )
+    else: 
+        model_scenario = ModelScenario(
+            obs=obs_data,
+            footprint=footprint_data,
+            flux=flux_dict,
+            bc=bc_data,
+        )
 
     # TODO: should we make this option explicit? Multiple fluxes can be stacked and used as a single flux
     split_by_sectors = len(flux_dict) > 1
