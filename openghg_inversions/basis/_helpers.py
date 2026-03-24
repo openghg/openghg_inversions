a3332f868fd9bbc95f102eba67c61cd0f8796965
SutarPrasad <vq21425@bristol.ac.uk>
2026-03-24 11:44:54 +0000

    writing back to fp_and_data from copied

--- openghg_inversions/basis/_helpers.py
+++ openghg_inversions/basis/_helpers.py
@@ -89,6 +89,7 @@ def fp_sensitivity(fp_and_data: dict, basis_func: xr.DataArray | dict[str, xr.Da
         
         # TODO: store houter hinner here
         root_ds["H"] = (sensitivity.dims, sensitivity.data, {"long_name": "sensitivity"})
+        fp_and_data[site].ds = root_ds
     return fp_and_data
 
 
